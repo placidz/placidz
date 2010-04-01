e842830ed765416887dc09d0723a64a0ded81787
florian.blanc.fr <florian.blanc.fr@50db6110-248a-11df-92e6-556e66e75246>
2010-04-01 20:28:19 +0000

    VS_TP4_OK

--- Etudes/Master_1/Semestre_2/Visualisation_Scientifique/tp/tp04/tools.h
+++ Etudes/Master_1/Semestre_2/Visualisation_Scientifique/tp/tp04/tools.h
@@ -1,8 +1,12 @@
 #include "Point2D.h"
 #include <algorithm>
+#include <math.h>
 #include <GL/glut.h>
 
 Point2D computeIntersection(Point2D a, Point2D b, Point2D c, Point2D d);
 void sortByAscOrder(int *t, int size);
 void sortByDescOrder(int *t, int size);
 void displayText(float x, float y, float z, void* font, const char* s);
+int roundUpToInt(float _val);
+void setPixel(int _x, int _y);
+void drawBresenhamLine(Point2D p1, Point2D p2);
