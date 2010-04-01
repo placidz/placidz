3eaefaa37a3f1c0b4c683b2ef8bc01f8c493ea77
florian.blanc.fr <florian.blanc.fr@50db6110-248a-11df-92e6-556e66e75246>
2010-04-01 20:28:19 +0000

    VS_TP4_OK

--- trunk/Etudes/Master_1/Semestre_2/Visualisation_Scientifique/tp/tp04/tools.cpp
+++ trunk/Etudes/Master_1/Semestre_2/Visualisation_Scientifique/tp/tp04/tools.cpp
@@ -60,3 +60,213 @@ void displayText(float x, float y, float z, void* font, const char* s)
 		s++;
 	}
 }
+
+int roundUpToInt(float _val)
+{
+    _val += 0.5;
+    return (floor(_val));
+}
+
+void setPixel(int _x, int _y)
+{
+    glBegin(GL_POINTS);
+	  glVertex2i(_x, _y);
+    glEnd();
+}
+
+void drawBresenhamLine(Point2D p1, Point2D p2)
+{
+    glColor3f(0.0, 1.0, 0.0);
+    int x1 = p1.x, y1 = p1.y, x2 = p2.x, y2 = p2.y;
+    int dx, dy;
+
+    if((dx = x2 - x1) != 0)
+    {
+	  if (dx > 0)
+	  {
+		if ((dy = y2 - y1) != 0)
+		{
+		    if (dy > 0)
+		    {
+			  if (dx >= dy)
+			  {
+				int e;
+				dx = (e = dx) * 2;
+				dy *= 2;
+				while (1)
+				{
+				    setPixel(x1, y1);
+				    if (x1++ == x2) break;
+				    if ((e = e - dy) < 0)
+				    {
+					  y1++;
+					  e += dx;
+				    }
+				}
+			  }
+			  else
+			  {
+				int e;
+				dy = (e = dy) * 2;
+				dx *= 2;
+				while (1)
+				{
+				    setPixel(x1, y1);
+				    if (y1++ == y2) break;
+				    if ((e = e - dx) < 0)
+				    {
+					  x1++;
+					  e += dy;
+				    }
+				}
+			  }
+		    }
+		    else
+		    {
+			  if (dx >= -dy)
+			  {
+				int e;
+				dx = (e = dx) * 2;
+				dy *= 2;
+				while (1)
+				{
+				    setPixel(x1, y1);
+				    if (x1++ == x2) break;
+				    if ((e = e + dy) < 0)
+				    {
+					  y1--;
+					  e += dx;
+				    }
+				}
+			  }
+			  else
+			  {
+				int e;
+				dy = (e = dy) * 2;
+				dx *= 2;
+				while (1)
+				{
+				    setPixel(x1, y1);
+				    if (y1-- == y2) break;
+				    if ((e = e + dx) > 0)
+				    {
+					  x1++;
+					  e += dy;
+				    }
+				}
+			  }
+		    }
+		}
+		else
+		{
+		    do
+		    {
+			  setPixel(x1, y1);
+		    } while (x1++ != x2);
+		}
+	  }
+	  else
+	  {
+		if ((dy = y2 - y1) != 0)
+		{
+		    if (dy > 0)
+		    {
+			  if (-dx >= dy)
+			  {
+				int e;
+				dx = (e = dx) *2;
+				dy *= 2;
+				while (1)
+				{
+				    setPixel(x1, y1);
+				    if (x1-- == x2) break;
+				    if ((e = e + dy) >= 0)
+				    {
+					  y1++;
+					  e+= dx;
+				    }
+				}
+			  }
+			  else
+			  {
+				int e;
+				dy = (e = dy) * 2;
+				dx *= 2;
+				while (1)
+				{
+				    setPixel(x1, y1);
+				    if (y1++ == y2) break;
+				    if ((e = e + dx) <= 0)
+				    {
+					  x1--;
+					  e += dy;
+				    }
+				}
+			  }
+		    }
+		    else
+		    {
+			  if (dx <= dy)
+			  {
+				int e;
+				dx = (e = dx) * 2;
+				dy *= 2;
+				while (1)
+				{
+				    setPixel(x1, y1);
+				    if (x1-- == x2) break;
+				    if ((e = e - dy) >= 0)
+				    {
+					  y1--;
+					  e += dx;
+				    }
+				}
+			  }
+			  else
+			  {
+				int e;
+				dy = (e = dy) * 2;
+				dx *= 2;
+				while (1)
+				{
+				    setPixel(x1, y1);
+				    if (y1-- == y2) break;
+				    if ((e = e - dx) >= 0)
+				    {
+					  x1--;
+					  e += dy;
+				    }
+				}
+			  }
+		    }
+		}
+		else
+		{
+		    do
+		    {
+			  setPixel(x1, y1);
+		    } while (x1-- != x2);
+		}
+	  }
+    }
+    else
+    {
+	  if ((dy = y2 - y1) != 0)
+	  {
+		if (dy > 0)
+		{
+		    do
+		    {
+			  setPixel(x1, y1);
+		    } while (y1++ != y2);
+		}
+		else
+		{
+		    do
+		    {
+			  setPixel(x1, y1);
+		    } while (y1-- != y2);
+		}
+	  }
+    }
+}
--- trunk/Etudes/Master_1/Semestre_2/Visualisation_Scientifique/tp/tp04/tp04.cpp
+++ trunk/Etudes/Master_1/Semestre_2/Visualisation_Scientifique/tp/tp04/tp04.cpp
@@ -13,71 +13,173 @@
 
 using namespace std;
 
+
+// DEFINES
+
 #define wWidth 800
 #define wHeight 600
-
 #define PI 3.14159265
+#define MAX_POINTS 24
 
+
+// DECLARATIONS VARIABLES
+
+GLuint idBaseDL = 0;
 float bg_color[] = {0.8, 0.8, 0.8, 1.0};
 
 int f1, f2, f3;
 int mouseX, mouseY;
 
+vector<Point2D> ListePoints;
+bool bAjoutPoint = 0;
+bool bTraceNaif = 1;
+bool bTraceBresenham = 0;
 
-void affichage (void)
+
+// CORPS DES FONCTIONS
+
+
+
+
+void tracer_naif(Point2D p1, Point2D p2)
+{
+    int x1, y1, x2, y2;
+    if (p1.x < p2.x)
+    {
+	  x1 = p1.x;
+	  y1 = p1.y;
+	  x2 = p2.x;
+	  y2 = p2.y;
+    }
+    else
+    {
+	  x1 = p2.x;
+	  y1 = p2.y;
+	  x2 = p1.x;
+	  y2 = p1.y;
+    }
+
+    int dx = x2 - x1;
+    int dy = y2 - y1;
+    glColor3f(0.0, 0.0, 1.0);
+    glBegin(GL_POINTS);
+    for (int x=x1; x < x2; x++)
+    {
+	  int y = y1 + dy * (x - x1) / dx;
+	  glVertex2i(x, y);
+    }
+    glEnd();
+}
+
+void drawPoints(void)
+{
+    glPointSize(3.0);
+    glColor3f(1.0, 0.0, 0.0);
+    glBegin(GL_POINTS);
+    for (int i=0; i<(int)ListePoints.size(); i++)
+    {
+	  glVertex2i(ListePoints[i].x, ListePoints[i].y);
+    }
+    glEnd();
+    glPointSize(1.0);
+}
+
+void drawBase(void)
+{
+    glColor3f(0.0, 0.0, 0.0);
+    glBegin(GL_LINES);
+    // Axe X
+    glVertex2i(0, 0);
+    glVertex2i(100, 0);
+    // Axe Y
+    glVertex2i(0, 0);
+    glVertex2i(0, 100);
+    glEnd();
+}
+
+void displayGL(void)
 {
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glCallList(idBaseDL);
+    if (!ListePoints.empty())
+    {
+	  for (int i=0; i<(int)ListePoints.size()-1; i+=2)
+	  {
+		if (bTraceNaif) tracer_naif(ListePoints[i], ListePoints[i+1]);
+		if (bTraceBresenham) drawBresenhamLine(ListePoints[i], ListePoints[i+1]);
+	  }
+	  drawPoints();
+    }
 
     glutSwapBuffers();
 }
 
-void redim (int width, int height)
+void reshapeGL(int _width, int _height)
 {
-    glViewport(0, 0, width, height);
+    glViewport(0, 0, _width, _height);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluOrtho2D(0, width, 0, height);
+    gluOrtho2D(-_width/2, _width/2, -_height/2, _height/2);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 }
 
-void clavier (unsigned char key, int x, int y)
+void keyboardGL(unsigned char _key, int _x, int _y)
 {
-    switch (key)
+    switch (_key)
     {
-	case 27:
-	exit(0);
-	break;
+    case 27:
+	  exit(0);
+	  break;
+
+    case 'e':
+	  ListePoints.clear();
+	  break;
+
+    case 'n':
+	  bTraceNaif = !bTraceNaif;
+	  break;
+
+    case 'b':
+	  bTraceBresenham = !bTraceBresenham;
+	  break;
 
     }
     glutPostRedisplay();
 }
 
-void souris (int button, int state, int x, int y)
+void mouseGL(int _button, int _state, int _x, int _y)
 {
-    y = wHeight - y;
+    _x -= wWidth/2;
+    _y = wHeight/2 - _y;
 
-    switch(button)
+    switch(_button)
     {
-	case GLUT_LEFT_BUTTON:
-        if (state == GLUT_DOWN)
+    case GLUT_LEFT_BUTTON:
+	  if (_state == GLUT_DOWN)
         {
+		if(!bAjoutPoint && ListePoints.size() < MAX_POINTS)
+		{
+		    ListePoints.push_back(Point2D(_x, _y));
+		    bAjoutPoint = 1;
+		}
 
         }
-        if (state == GLUT_UP)
+	  if (_state == GLUT_UP)
         {
-
+		bAjoutPoint = 0;
         }
         break;
 
 	case GLUT_MIDDLE_BUTTON:
-        if (state == GLUT_DOWN)
+	  if (_state == GLUT_DOWN)
         {
         }
         break;
 
 	case GLUT_RIGHT_BUTTON:
-        if (state == GLUT_DOWN)
+	  if (_state == GLUT_DOWN)
         {
         }
         break;
@@ -100,7 +202,27 @@ void passiveMotionGL(int _x, int _y)
     glutPostRedisplay();
 }
 
-int main (int argc, char **argv)
+GLuint createBaseDL()
+{
+    GLuint id;
+    id = glGenLists(1);
+    if (!id) return id;
+
+    glNewList(id, GL_COMPILE);
+    drawBase();
+    glEndList();
+
+    return id;
+}
+
+void initGL(void)
+{
+    glClearColor(bg_color[0], bg_color[1], bg_color[2], bg_color[3]);
+
+    idBaseDL = createBaseDL();
+}
+
+int main(int argc, char **argv)
 {
     glutInit(&argc, argv);
 
@@ -109,15 +231,14 @@ int main (int argc, char **argv)
     glutInitWindowPosition(200, 100);
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
     f1 = glutCreateWindow("[TP4] Visualisation Scientifique - Trace de segments : Algorithme naif et Bresenham");
-    glutDisplayFunc(affichage);
-    glutReshapeFunc(redim);
-    glutKeyboardFunc(clavier);
-    glutMouseFunc(souris);
+    glutDisplayFunc(displayGL);
+    glutReshapeFunc(reshapeGL);
+    glutKeyboardFunc(keyboardGL);
+    glutMouseFunc(mouseGL);
     glutMotionFunc(motionGL);
     glutPassiveMotionFunc(passiveMotionGL);
 
-    glClearColor(bg_color[0], bg_color[1], bg_color[2], bg_color[3]);
-    //glPointSize(5);
+    initGL();
 
     glutMainLoop();
 
